32bb691f053d6bb13436226e8610245c3dd8841f
Atharva45 <atharvaphanse403@gmail.com>
2025-12-31 13:42:04 +0530

    storing user details in db

--- components/header.jsx
+++ components/header.jsx
@@ -8,8 +8,11 @@ import { UserButton } from "@clerk/nextjs";
 import { Button } from "./ui/button";
 import { Authenticated, Unauthenticated } from "convex/react";
 import {BarLoader} from "react-spinners";
+import { useStoreUser } from "@/hooks/use-store-user";
 
 const Header = () => {
+  const {isLoading} = useStoreUser();
+
   return (
     <>
       <nav className="fixed top-0 left-0 right-0 bg-background/80 backdrop-blur-xl z-20 border-b">
@@ -44,9 +47,11 @@ const Header = () => {
         {/* Mobile serch location - below navbar  */}
 
         {/* Loader  */}
-        <div className="absolute bottom-0 left-0 w-full">
+        {isLoading &&(
+          <div className="absolute bottom-0 left-0 w-full">
           <BarLoader width={"100%"} color="#a855f7"/> 
         </div>
+      )}
       </nav>
 
       {/* Modals  */}
